2c17f3a1cbf1e4e773659a75c5fb286b10d11026
maxg <max.goettner@vw-fc.com>
2020-01-17 19:01:30 +0100

    .

--- docs/config.js
+++ docs/config.js
@@ -3,7 +3,7 @@ var config = {
   USE_LOCATION : true,
   SHOW_STOPS : false,
   startPosition : [52.4041, 13.073],
-  mapbox_api_key : "pk.eyJ1IjoibWF4Z2VuZSIsImEiOiJjazNhY2Vob3QwYXd2M2NreDk5d3dvcWh0In0.7EyDKC6KYi9yfhlsvErjUA",
+  mapbox_api_key : "pk.eyJ1IjoibWF4Z2VuZSIsImEiOiJjazVpOHhva2gwOG1pM21tbjM1MGZ0MWdjIn0.YcaRguzrCDmrBdhCfohvpA",
   COMPASS_OFFSET : 0
 }
 
